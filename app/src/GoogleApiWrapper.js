870dcecf13828b5115ba77e4205c50dd04a0a36d
Quentin Da Silva <quentin_51@live.fr>
2018-10-30 14:22:48 +0100

    add marker with image

--- app/src/GoogleApiWrapper.js
+++ app/src/GoogleApiWrapper.js
@@ -1,34 +1,72 @@
-import React, { Component } from 'react'
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
 export class MapContainer extends Component {
   render() {
     return (
-      <Map google={this.props.google}
+      <Map
+        google={this.props.google}
         initialCenter={{
           lat: 49.256983,
           lng: 4.019755
         }}
         zoom={15}
-        onClick={this.onMapClicked}>
-
-        <Marker onClick={this.onMarkerClick}
-          name={'Current location'}
+        onClick={this.onMapClicked}
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={"Current location"}
           icon={{
             url: "./img/pumpkin.png",
-            anchor: new this.props.google.maps.Point(16, 16),
-            scaledSize: new this.props.google.maps.Size(32, 32)
-          }} />
-
+            anchor: new this.props.google.maps.Point(60, 60),
+            scaledSize: new this.props.google.maps.Size(60, 60)
+          }}
+        />
         <Marker
-          name={'Gare de Reims'}
+          name={"Gare de Reims"}
           position={{ lat: 49.258919, lng: 4.024525 }}
           icon={{
-            url: "./img/pumpkin.png",
-            anchor: new this.props.google.maps.Point(16, 16),
-            scaledSize: new this.props.google.maps.Size(32, 32)
-          }} />
-
+            url: "./img/candy.png",
+            anchor: new this.props.google.maps.Point(60, 60),
+            scaledSize: new this.props.google.maps.Size(60, 60)
+          }}
+        />
+        <Marker
+          name={"Cathédrale Notre-Dame de Reims"}
+          position={{ lat: 49.253878, lng: 4.034093 }}
+          icon={{
+            url: "./img/candy.png",
+            anchor: new this.props.google.maps.Point(60, 60),
+            scaledSize: new this.props.google.maps.Size(60, 60)
+          }}
+        />
+        <Marker
+          name={"Hôtel de ville"}
+          position={{ lat: 49.258175, lng: 4.032134 }}
+          icon={{
+            url: "./img/candy.png",
+            anchor: new this.props.google.maps.Point(60, 60),
+            scaledSize: new this.props.google.maps.Size(60, 60)
+          }}
+        />
+        <Marker
+          name={"Place Royale"}
+          position={{ lat: 49.255585, lng: 4.034319 }}
+          icon={{
+            url: "./img/candy.png",
+            anchor: new this.props.google.maps.Point(60, 60),
+            scaledSize: new this.props.google.maps.Size(60, 60)
+          }}
+        />
+        <Marker
+          name={"Fontaine Subé"}
+          position={{ lat: 49.255147, lng: 4.027244 }}
+          icon={{
+            url: "./img/candy.png",
+            anchor: new this.props.google.maps.Point(60, 60),
+            scaledSize: new this.props.google.maps.Size(60, 60)
+          }}
+        />
         <InfoWindow onClose={this.onInfoWindowClose}>
           <div>
             <h1>Reims</h1>
@@ -41,4 +79,4 @@ export class MapContainer extends Component {
 
 export default GoogleApiWrapper({
   apiKey: "AIzaSyCeDmh4OYEnE1NDKhAu1u66yDln-AQXEyM"
-})(MapContainer)
+})(MapContainer);
